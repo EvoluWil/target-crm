ffccfa8e8f3407dbb1b56d1dfbe37669646d1658
Willian Rodrigues <willian.rsilva1994@gmail.com>
2021-11-24 09:15:49 -0300

    api url

--- frontend/src/data/services/ServiceApi.ts
+++ frontend/src/data/services/ServiceApi.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 //DECLARA O AXIOS PARA USAR O BACK END NA PORTA 3333 OU EM UMA PASSA POR PROD
 export const serviceApi = axios.create({
-  baseURL: process.env.NEXT_APP_URL,
+  baseURL: https://api-target.herokuapp.com,
 });
